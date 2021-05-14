37f334ba05b509e6923de6b76ac6a21d67a1c25f
Nicoll Oliver <nicolldon96@gmail.com>
2021-05-13 21:51:22 -0400

    commit tree 75948752db73d4ffce7a35a679a162ec2e08e7ef

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max( 5, 10, 15, 20);
